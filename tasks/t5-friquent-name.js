3128026e79f3aff34e5b05dbd7e9be39b0131e97
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2024-06-20 23:58:46 +0300

    added tests and validation for task: friquent name

--- tasks/t5-friquent-name.js
+++ tasks/t5-friquent-name.js
@@ -3,9 +3,20 @@
 // input: ["Alex", "Bob", "Alex"] - output: "Alex"
 
 const usersList = ['Alex', 'Bob', 'Alex', 'Ann', 'James'];
+
 const friquentName = (nameList) => {
+  //additional validation
+  if (nameList.length === 0) {
+    return 'Your list is empty';
+  }
+
   const result = nameList.reduce(
     (acc, name) => {
+      //additional validation (undefined, null)
+      if (name === undefined || name === null) {
+        return acc;
+      }
+
       if (acc.counts[name] === undefined) {
         acc.counts[name] = 1;
       } else {
@@ -20,6 +31,11 @@ const friquentName = (nameList) => {
     },
     { counts: {}, maxCount: 0, friquentName: '' }
   );
+
+  if (result.maxCount === 0) {
+    return "Your list doesn't have names";
+  }
+
   return result.friquentName;
 };
 
--- tests/t5-friquent-name.test.js
+++ tests/t5-friquent-name.test.js
@@ -7,6 +7,26 @@ describe('Correct list of names', () => {
       argument: ['Ann', 'John', 'Bob', 'Ann', 'Ann'],
       expectedName: 'Ann',
     },
+    {
+      testName: 'Empty array',
+      argument: [],
+      expectedName: 'Your list is empty',
+    },
+    {
+      testName: 'value in array - undefined',
+      argument: ['Ann', 'John', undefined, null, 'Bob', 'Bob'],
+      expectedName: 'Bob',
+    },
+    {
+      testName: 'value in array - null',
+      argument: ['Ann', 'John', null, 'Bob', 'Bob'],
+      expectedName: 'Bob',
+    },
+    {
+      testName: 'all values in array - undefined and null',
+      argument: [null, undefined, null],
+      expectedName: "Your list doesn't have names",
+    },
   ];
 
   testCases.forEach((test) => {
@@ -19,5 +39,5 @@ describe('Correct list of names', () => {
 });
 
 //разобрать случаи с undefined / null
-//разобрать случай с пустым массивом
+
 //разобрать случай что все имена разные то ответ приходит кокоето сообщение
