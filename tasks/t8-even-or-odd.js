7a51ae1ca9fd5041f3aec1f1e4c2e7a82069a4db
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2024-08-19 20:01:24 +0300

    Resolve t8
    resolved task8: Even or Odd

---
+++ tasks/t8-even-or-odd.js
@@ -0,0 +1,10 @@
+//Create a function that takes an integer as an argument
+//and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOdd(number) {
+  return number % 2 === 0 ? 'Even' : 'Odd';
+}
+
+module.exports = {
+  evenOdd,
+};
