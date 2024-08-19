f29ed81f7f8c20f3e65f94d53795cbacd0a2680e
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2024-08-19 20:14:33 +0300

    testing task8
    tested different integers

---
+++ tests/t8-even-or-odd.test.js
@@ -0,0 +1,39 @@
+const { evenOdd } = require('../tasks/t8-even-or-odd');
+
+describe('Must return "Even" or "Odd"', () => {
+  const testCases = [
+    {
+      name: 'number 12 must return "Even"',
+      argument: 12,
+      expected: 'Even',
+    },
+    {
+      name: 'number 3 must return "Even"',
+      argument: 3,
+      expected: 'Odd',
+    },
+    {
+      name: 'number 0 must return "Even"',
+      argument: 0,
+      expected: 'Even',
+    },
+    {
+      name: 'number -11 must return "Even"',
+      argument: -11,
+      expected: 'Odd',
+    },
+    {
+      name: 'number -4 must return "Even"',
+      argument: -4,
+      expected: 'Even',
+    },
+  ];
+
+  testCases.forEach((test) => {
+    it(test.name, () => {
+      const testedFunc = evenOdd(test.argument);
+
+      expect(testedFunc).toBe(test.expected);
+    });
+  });
+});
