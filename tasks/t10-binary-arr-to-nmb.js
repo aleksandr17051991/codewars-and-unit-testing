69fe55f762fcc0dc987b7f531402b1f63ef6b83a
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2024-08-21 23:19:10 +0300

    Solution task10
    solved binary task with parseInt

---
+++ tasks/t10-binary-arr-to-nmb.js
@@ -0,0 +1,12 @@
+//Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+//Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+// [0, 1, 0, 1] ==> 5
+// [0, 0, 0, 1] ==> 1
+
+//However, the arrays can have varying lengths, not just limited to 4.
+
+const binaryArrayToNumber = (arr) => {
+  return parseInt(arr.join(''), 2);
+};
