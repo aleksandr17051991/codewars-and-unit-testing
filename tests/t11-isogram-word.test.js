f1fe31b852158cdb093d6139260b7e3930ca0566
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2024-09-27 22:57:27 +0300

    testing
    tested isogram and not isogram words

--- tests/t11-isogram-word.test.js
+++ tests/t11-isogram-word.test.js
@@ -1,3 +1,47 @@
 const { isIsogram } = require('../tasks/t11-isogram-word');
 
-describe('Correct isograms', () => {});
+describe('Correct isograms', () => {
+  const testCases = [
+    {
+      name: `"olEksAndr" is isogram`,
+      argument: 'olEksAndr',
+    },
+    {
+      name: `empty string is isogram`,
+      argument: '',
+    },
+    {
+      name: `"ASx79" is isogram`,
+      argument: 'ASx79',
+    },
+  ];
+
+  testCases.forEach((test) => {
+    const testedFunc = isIsogram(test.argument);
+
+    it(test.name, () => {
+      expect(testedFunc).toBeTruthy();
+    });
+  });
+});
+
+describe('Incorrect isograms', () => {
+  const testCases = [
+    {
+      name: `"moOuse" isn't isogram`,
+      argument: 'moOuse',
+    },
+    {
+      name: `"Anna" isn't isogram`,
+      argument: 'Anna',
+    },
+  ];
+
+  testCases.forEach((test) => {
+    const testedFunc = isIsogram(test.argument);
+
+    it(test.name, () => {
+      expect(testedFunc).toBeFalsy();
+    });
+  });
+});
